5382c7eb09197d1a8373804cd3b894522c030898
User <gorbatovskiy.igor@gmail.com>
2024-01-19 23:15:08 +0300

    feat: finish 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { readdir } = require('node:fs/promises');
+const { createWriteStream, createReadStream } = require('fs');
+const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
+const stylesDir = path.join(__dirname, 'styles');
+
+readdir(stylesDir, { withFileTypes: true })
+  .then((files) => {
+    const writer = createWriteStream(bundle);
+
+    files.forEach((el) => {
+      const file = path.join(el.path, el.name);
+      const fileExt = path.parse(file).ext.slice(1);
+
+      if (el.isFile() && fileExt === 'css') {
+        const style = path.join(stylesDir, el.name);
+        const reader = createReadStream(style, 'utf-8');
+        reader.pipe(writer);
+      }
+    });
+  })
+  .catch((err) => console.error(err));
